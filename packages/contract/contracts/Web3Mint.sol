39e7221a645c3c1885f0fbed449465e8eacd5693
toyop-GsF9 <a3saki4yume@gmail.com>
2023-08-28 12:21:08 +0900

    test

--- packages/contract/contracts/Web3Mint.sol
+++ packages/contract/contracts/Web3Mint.sol
@@ -1,42 +1,65 @@
 // Web3Mint.sol
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
-// いくつかの OpenZeppelin のコントラクトをインポートします。
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+//OpenZeppelinが提供するヘルパー機能をインポートします。
 import "@openzeppelin/contracts/utils/Counters.sol";
+
+import "./libraries/Base64.sol";
 import "hardhat/console.sol";
 
-// インポートした OpenZeppelin のコントラクトを継承しています。
-// 継承したコントラクトのメソッドにアクセスできるようになります。
-contract Web3Mint is ERC721URIStorage {
-    // OpenZeppelin が tokenIds を簡単に追跡するために提供するライブラリを呼び出しています
+contract Web3Mint is ERC721 {
+    struct NftAttributes {
+        string name;
+        string imageURL;
+    }
+
+    NftAttributes[] Web3Nfts;
+
     using Counters for Counters.Counter;
-    // _tokenIdsを初期化（_tokenIds = 0）
+    // tokenIdはNFTの一意な識別子で、0, 1, 2, .. N のように付与されます。
     Counters.Counter private _tokenIds;
 
-    // NFT トークンの名前とそのシンボルを渡します。
-    constructor() ERC721("ToyopNFT", "TOYOP") {
+    constructor() ERC721("NFT", "nft") {
         console.log("This is my NFT contract.");
     }
 
     // ユーザーが NFT を取得するために実行する関数です。
-    function makeAnEpicNFT() public {
-        // 現在のtokenIdを取得します。tokenIdは0から始まります。
+
+    function mintIpfsNFT(string memory name, string memory imageURI) public {
         uint256 newItemId = _tokenIds.current();
-        // msg.sender を使って NFT を送信者に Mint します。
         _safeMint(msg.sender, newItemId);
-        // NFT データを設定します。
-        _setTokenURI(
-            newItemId,
-            "https://api.jsonstorage.net/v1/json/6e024d00-dc38-43f5-b156-d217e04c2576/983162ab-0478-430b-966c-618a4379e05d"
-        );
-        // NFTがいつ誰に作成されたかを確認します。
+        Web3Nfts.push(NftAttributes({name: name, imageURL: imageURI}));
         console.log(
             "An NFT w/ ID %s has been minted to %s",
             newItemId,
             msg.sender
         );
-        // 次の NFT が Mint されるときのカウンターをインクリメントする。
         _tokenIds.increment();
+    }
+
+    function tokenURI(
+        uint256 _tokenId
+    ) public view override returns (string memory) {
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        Web3Nfts[_tokenId].name,
+                        " -- NFT #: ",
+                        Strings.toString(_tokenId),
+                        '", "description": "An epic NFT", "image": "ipfs://',
+                        Web3Nfts[_tokenId].imageURL,
+                        '"}'
+                    )
+                )
+            )
+        );
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+        return output;
     }
 }
